8dd96ca0de45619a13847a37465d3eba3a58bc94
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-05-02 09:03:24 +0900

    Added navigation to appbar

---
+++ lib/routes/volume.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class VolumePage extends StatelessWidget {
+  const VolumePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Volume page");
+  }
+}
