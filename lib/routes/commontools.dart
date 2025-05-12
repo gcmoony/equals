98eaba6954254c4848ba521238184b676775e253
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-05-12 20:08:18 +0900

    Created generic class object for conversions

--- lib/routes/commontools.dart
+++ lib/routes/commontools.dart
@@ -39,7 +39,11 @@ class CommonToolsPage extends StatelessWidget {
                       builder: (BuildContext context) {
                         return Scaffold(
                           appBar: AppBar(title: const Text("Pound - Kilogram")),
-                          body: Text("Item 2 body"),
+                          body: ConversionItem(
+                            conversionRate: 0.45359237,
+                            firstUnitName: "Pound",
+                            secondUnitName: "Kilogram",
+                          ),
                         );
                       },
                     ),
@@ -67,6 +71,135 @@ class CommonToolsPage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class ConversionItem extends StatefulWidget {
+  const ConversionItem({
+    super.key,
+    required this.conversionRate,
+    required this.firstUnitName,
+    required this.secondUnitName,
+  });
+  final String firstUnitName;
+  final String secondUnitName;
+  final double conversionRate;
+
+  @override
+  State<ConversionItem> createState() => _ConversionItemState();
+}
+
+class _ConversionItemState extends State<ConversionItem> {
+  double firstUnitValue = 0;
+  double secondUnitValue = 0;
+  String errMsg1 = "";
+  String errMsg2 = "";
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Card(
+            elevation: 4,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SelectionArea(
+                            child: Text(
+                              firstUnitValue.toStringAsFixed(3),
+                              style: Theme.of(context).textTheme.displaySmall,
+                              softWrap: false,
+                              overflow: TextOverflow.fade,
+                            ),
+                          ),
+                        ),
+                        Text(widget.firstUnitName),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(child: Center(child: Icon(Icons.multiple_stop))),
+                Expanded(
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SelectionArea(
+                            child: Text(
+                              secondUnitValue.toStringAsFixed(3),
+                              style: Theme.of(context).textTheme.displaySmall,
+                              softWrap: false,
+                              overflow: TextOverflow.fade,
+                            ),
+                          ),
+                        ),
+                        Text(widget.secondUnitName),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: "1",
+              helperText: widget.firstUnitName,
+            ),
+            onChanged:
+                (newFirstUnitValue) => setState(() {
+                  try {
+                    firstUnitValue = double.parse(newFirstUnitValue);
+                    errMsg1 = "";
+                  } catch (err) {
+                    errMsg1 = "Must be a valid number";
+                    firstUnitValue = 0;
+                  }
+                  secondUnitValue = firstUnitValue * widget.conversionRate;
+                }),
+          ),
+        ),
+        Text(errMsg1),
+
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: TextField(
+            // controller: TextEditingController(text: centimeters.toString()),
+            decoration: InputDecoration(
+              hintText: "2.54",
+              helperText: widget.secondUnitName,
+            ),
+            onChanged:
+                (newSecondUnitValue) => setState(() {
+                  try {
+                    secondUnitValue = double.parse(newSecondUnitValue);
+                    errMsg2 = "";
+                  } catch (err) {
+                    errMsg2 = "Must be a valid number";
+                    secondUnitValue = 0;
+                  }
+                  firstUnitValue = secondUnitValue / widget.conversionRate;
+                }),
+          ),
+        ),
+        Text(errMsg2),
+      ],
     );
   }
 }
