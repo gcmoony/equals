69cb1559778390c3a9caab6cb67ef673f65a4dbc
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-05-12 20:12:30 +0900

    Removed specific conversion object "inches to centimeters"

--- lib/routes/commontools.dart
+++ lib/routes/commontools.dart
@@ -11,7 +11,7 @@ class CommonToolsPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           ListTile(
-            title: const Text("Inch - Centimeters"),
+            title: const Text("Inch - Centimeter"),
             onTap:
                 () => {
                   Navigator.push(
@@ -20,9 +20,13 @@ class CommonToolsPage extends StatelessWidget {
                       builder: (BuildContext context) {
                         return Scaffold(
                           appBar: AppBar(
-                            title: const Text("Inch - Centimeters"),
+                            title: const Text("Inch - Centimeter"),
                           ),
-                          body: InchCentimeter(),
+                          body: ConversionItem(
+                            conversionRate: 2.54,
+                            firstUnitName: "Inch",
+                            secondUnitName: "Centimeter",
+                          ),
                         );
                       },
                     ),
@@ -195,125 +199,6 @@ class _ConversionItemState extends State<ConversionItem> {
                     secondUnitValue = 0;
                   }
                   firstUnitValue = secondUnitValue / widget.conversionRate;
-                }),
-          ),
-        ),
-        Text(errMsg2),
-      ],
-    );
-  }
-}
-
-class InchCentimeter extends StatefulWidget {
-  const InchCentimeter({super.key});
-  @override
-  State<InchCentimeter> createState() => _InchCentimeterState();
-}
-
-class _InchCentimeterState extends State<InchCentimeter> {
-  double inches = 0;
-  double centimeters = 0;
-  String errMsg1 = "";
-  String errMsg2 = "";
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Card(
-            elevation: 4,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Expanded(
-                  child: Center(
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SelectionArea(
-                            child: Text(
-                              inches.toStringAsFixed(3),
-                              style: Theme.of(context).textTheme.displaySmall,
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                        ),
-                        Text("Inches"),
-                      ],
-                    ),
-                  ),
-                ),
-                Expanded(child: Center(child: Icon(Icons.multiple_stop))),
-                Expanded(
-                  child: Center(
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: SelectionArea(
-                            child: Text(
-                              centimeters.toStringAsFixed(3),
-                              style: Theme.of(context).textTheme.displaySmall,
-                              softWrap: false,
-                              overflow: TextOverflow.fade,
-                            ),
-                          ),
-                        ),
-                        Text("Centimeters"),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-
-        Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: TextField(
-            // controller: TextEditingController(text: inches.toString()),
-            decoration: InputDecoration(hintText: "1", helperText: "Inches"),
-            onChanged:
-                (newInches) => setState(() {
-                  try {
-                    inches = double.parse(newInches);
-                    errMsg1 = "";
-                  } catch (err) {
-                    errMsg1 = "Must be a valid number";
-                    inches = 0;
-                  }
-                  centimeters = inches * 2.54;
-                }),
-          ),
-        ),
-        Text(errMsg1),
-
-        Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: TextField(
-            // controller: TextEditingController(text: centimeters.toString()),
-            decoration: InputDecoration(
-              hintText: "2.54",
-              helperText: "Centimeters",
-            ),
-            onChanged:
-                (newCentimeters) => setState(() {
-                  try {
-                    centimeters = double.parse(newCentimeters);
-                    errMsg2 = "";
-                  } catch (err) {
-                    errMsg2 = "Must be a valid number";
-                    inches = 0;
-                  }
-                  inches = centimeters / 2.54;
                 }),
           ),
         ),
