5f8c115ae0e6722ca1cc173e5043e0fadace1191
GCMoony <47188474+GCMoony@users.noreply.github.com>
2025-05-13 20:47:33 +0900

    Added new route

---
+++ lib/routes/lengths.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LengthsPage extends StatelessWidget {
+  const LengthsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Lengths");
+  }
+}
